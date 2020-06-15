a3549dd3e61bfc37dc8d962b4d8b621454ae6617
wangmingyue <448335044@qq.com>
2020-06-15 18:01:24 +0800

    项目初始化

---
+++ blog/docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    title: '老王的博客',
+    description: '爱生活，爱老王',
+
+    themeConfig:{
+        nav: [{text: "主页", link: "/"      },
+            { text: "html", link: "/node/" },
+            { text: "css", link: "/webframe/"},
+            { text: "js", link: "/database/"   },
+            { text: "vue", link: "/android/"   },
+            { text: "面试", link: "/interview/" }
+          ],
+      }
+      
+      
+}
+
